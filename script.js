bac942a56da2b3aad5f9f998e7cab2fcad135e68
h0tambek <51804825+h0tambek@users.noreply.github.com>
2022-12-12 14:09:09 -0600

    Functioning UI that does nothing

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-    const goat = document.getElementsByClassName('goat');
+// Wait for the HTML page to load
+    // Get the button by its ID
+var button = document.querySelector('#contact');
+var close = document.querySelector('.close-button')
 
-    document.addEventListener('DOMContentLoaded', () => {
-    document.addEventListener('mousemove', (event) => {
-        const x = event.clientX;
-        const y = event.clientY;
+// Add an event listener to the button
+button.addEventListener('click', function() {
+  // Get the pop-up element by its ID
+  var popUp = document.querySelector('#popup');
 
-        goat.style.position = 'absolute';
-        goat.style.left = x + 1+ 'px';
-        goat.style.top = y + 1 + 'px';
+  // Change the pop-up's display property to 'block'
+  popUp.style.display = 'block';
+});
+
+
+    // Get the text element
+    var text = document.querySelector('.goat');
+  
+    // Add an event listener that updates the text's position
+    // when the user moves their mouse
+    document.addEventListener('mousemove', function(event) {
+      text.style.top = event.clientY + 'px';
+      text.style.left = event.clientX + 'px';
     });
-    });
+    const mouseFollow = document.querySelector('.goat');
 
-                
-    const popup = document.getElementById('popup');
-    const showPopupBtn = document.querySelector('button[id="contact]');
-
-
+document.addEventListener('mousemove', (e) => {
+  mouseFollow.style.cssText = `
+    left: ${e.clientX - 5}px;
+    top:  ${e.clientY - 5}px;
+  `;
+});
+    
+  
+  
